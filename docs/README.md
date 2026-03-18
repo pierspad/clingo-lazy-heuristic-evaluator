345c0c4c6c5e9b968a7476f12ac5c64496649b9f
pierspad <pierpaolospadafora23+github@gmail.com>
2026-03-18 17:53:24 +0100

    dai

--- docs/README.md
+++ docs/README.md
@@ -11,5 +11,8 @@ source .venv/bin/activate
 pip install -r requirements.txt
 ```
 
-clingo currently has a bug, to patch it 
+clingo currently has a bug, to patch and build it from the `clingo-modified` directory, run the following commands from the root of the project:
 
+```bash
+sh ./patch_and_build_clingo/setup_clingo.sh
+```
--- patch_and_build_clingo/setup_clingo.sh
+++ patch_and_build_clingo/setup_clingo.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 set -e
 
-# 1. Clone shallow della repository
-git clone --depth 1 https://github.com/potassco/clingo.git clingo-repo
-cd clingo-repo
+# Nome della directory di lavoro
+TARGET_DIR="clingo-modified"
 
-# 2. Inizializzazione dei sottomoduli
-git submodule update --init --recursive
+# Rimuove la directory solo se esiste per garantire un ambiente pulito
+[ -d "$TARGET_DIR" ] && rm -rf "$TARGET_DIR"
 
-# 3. Applicazione della patch
-patch -p1 < ../fix.patch
+# 1. Clone shallow: scarica solo l'ultimo commit per risparmiare tempo
+git clone --depth 1 https://github.com/potassco/clingo.git "$TARGET_DIR"
+cd "$TARGET_DIR"
 
-# 4. Creazione dell'ambiente di compilazione
+# 2. Inizializzazione shallow dei sottomoduli
+# Essenziale per scaricare clasp e le altre dipendenze in modo efficiente
+git submodule update --init --recursive --depth 1
+
+# 3. Applicazione della patch per le euristiche
+# Assicurati che fix.patch sia nella directory superiore
+if [ -f ../fix.patch ]; then
+    patch -p1 < ../fix.patch
+fi
+
+# 4. Configurazione dell'ambiente di build
 mkdir -p build
 cd build
 
-# 5. Configurazione e generazione dei file di build
+# 5. Generazione dei file con Ninja e Clang++ (scelte ottime per lo sviluppo su Arch)
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ ..
 
 # 6. Compilazione
