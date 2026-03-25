a4a0de3aee22a45454973fd887bba869c5b78f94
pierspad <pierpaolospadafora23+github@gmail.com>
2026-03-25 15:57:45 +0100

    bene

--- test_folder/3_compare.sh
+++ test_folder/3_compare.sh
@@ -1,2 +1,7 @@
 # compare clingo 5.8.0 on classical BSP with
 # clingo-modified with lazy heuristics on the same problem, but with a non-ground heuristic directive
+
+
+clingo 5.8.0 __BSP.lp __.common_range.lp 
+
+~/Desktop/clingo-lazy-heuristics/clingo-modified/build/bin/clingo __BSP.lp __.common_range.lp _1_grounded.lp _2_non_ground.lp
